58681e8a4b218bc261e905feee0444f8b889f451
Juanes395 <ortizjuanes2017@gmail.com>
2025-11-19 21:36:38 -0500

    Tipo T

--- AgenciaViajess/src/Persistencia/CarroRepository.java
+++ AgenciaViajess/src/Persistencia/CarroRepository.java
@@ -5,10 +5,36 @@
  */
 package Persistencia;
 
-/**
- *
- * @author DELL
- */
+
+import Modelos.Carro;
+import java.util.List;
+import java.util.ArrayList;
+
 public class CarroRepository {
+    private IDataAccess<Carro> dataAccess;
     
+    public CarroRepository() {
+        this.dataAccess = new JsonRepository<>("carroa.json", Carro.class);
+    }
+    
+    // Constructor for dependency injection
+    public CarroRepository(IDataAccess<Carro> dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+    
+    public List<Carro> getAllCarros() {
+        return dataAccess.findAll();
+    }
+    
+    public Carro findCarroById(Integer id) {
+        return dataAccess.findById(id);
+    }
+    
+    public void saveCarro(Carro carro) {
+        dataAccess.save(carro);
+    }
+    
+    public void deleteCarro(Integer id) {
+        dataAccess.delete(id);
+    }
 }
