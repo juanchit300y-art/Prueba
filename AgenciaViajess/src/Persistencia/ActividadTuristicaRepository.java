b232454961e5099712f7b659fa4d5a2914267dfd
Juanes395 <ortizjuanes2017@gmail.com>
2025-11-19 21:47:44 -0500

    ko

--- AgenciaViajess/src/Persistencia/ActividadTuristicaRepository.java
+++ AgenciaViajess/src/Persistencia/ActividadTuristicaRepository.java
@@ -10,31 +10,13 @@ import Modelos.ActividadTuristica;
 import java.util.List;
 import java.util.ArrayList;
 
-public class ActividadTuristicaRepository {
-    private IDataAccess<ActividadTuristica> dataAccess;
+public class ActividadTuristicaRepository extends GeneralRepository<ActividadTuristica>{
+    
     
     public ActividadTuristicaRepository() {
         this.dataAccess = new JsonRepository<>("actividadesTuristicas.json", ActividadTuristica.class);
     }
-    
-    // Constructor for dependency injection
     public ActividadTuristicaRepository(IDataAccess<ActividadTuristica> dataAccess) {
         this.dataAccess = dataAccess;
-    }
-    
-    public List<ActividadTuristica> getAllActividadesTuristicas() {
-        return dataAccess.findAll();
-    }
-    
-    public ActividadTuristica findActividadTuristicaById(Integer id) {
-        return dataAccess.findById(id);
-    }
-    
-    public void saveActividadTuristica(ActividadTuristica actividadTuristica) {
-        dataAccess.save(actividadTuristica);
-    }
-    
-    public void deleteActividadTuristica(Integer id) {
-        dataAccess.delete(id);
     }
 }
