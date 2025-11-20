cf389b696c3f5bb74675ce3b00475caa579b386b
Juanes395 <ortizjuanes2017@gmail.com>
2025-11-19 22:55:01 -0500

    relacion lista

--- AgenciaViajess/src/Persistencia/ActividadTuristicaRepository.java
+++ AgenciaViajess/src/Persistencia/ActividadTuristicaRepository.java
@@ -7,6 +7,8 @@ package Persistencia;
 
 
 import Modelos.ActividadTuristica;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ActividadTuristicaRepository extends GeneralRepository<ActividadTuristica>{
     
@@ -16,5 +18,15 @@ public class ActividadTuristicaRepository extends GeneralRepository<ActividadTur
     }
     public ActividadTuristicaRepository(IDataAccess<ActividadTuristica> dataAccess) {
         this.dataAccess = dataAccess;
+    }
+    public List<ActividadTuristica> findActividadesTuristicasByMunicipioId(Integer municipioId) {
+        List<ActividadTuristica> ActividadesTuristicas = getAllT();
+        List<ActividadTuristica> result = new ArrayList<>();
+        for (ActividadTuristica actividadTuristica : ActividadesTuristicas) {
+            if (actividadTuristica.getIdMunicipio() != null && actividadTuristica.getIdMunicipio().equals(municipioId)) {
+                result.add(actividadTuristica);
+            }
+        }
+        return result;
     }
 }
