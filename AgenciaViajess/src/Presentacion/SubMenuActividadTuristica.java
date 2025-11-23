39289d19b179bf45031b02a4c611b31b93b80cc0
Juanes395 <ortizjuanes2017@gmail.com>
2025-11-22 20:00:52 -0500

    SubMenuActividadTuristica

--- AgenciaViajess/src/Presentacion/SubMenuActividadTuristica.java
+++ AgenciaViajess/src/Presentacion/SubMenuActividadTuristica.java
@@ -5,11 +5,38 @@
  */
 package Presentacion;
 import Controllers.ActividadTuristicaController;
+import java.util.Scanner;
 /**
  *
  * @author DELL
  */
 public class SubMenuActividadTuristica {
     private ActividadTuristicaController controlador;
+    Scanner scanner;
+
+    public SubMenuActividadTuristica() {
+    }
+
+    public SubMenuActividadTuristica(ActividadTuristicaController controlador, Scanner scanner) {
+        this.controlador = controlador;
+        this.scanner = scanner;
+    }
+    
+    /**
+     * @return the controlador
+     */
+    public ActividadTuristicaController getControlador() {
+        return controlador;
+    }
+
+    /**
+     * @param controlador the controlador to set
+     */
+    public void setControlador(ActividadTuristicaController controlador) {
+        this.controlador = controlador;
+    }
+    public void verSubMenuActividadTuristica(){
+        System.out.println();
+    }
     
 }
