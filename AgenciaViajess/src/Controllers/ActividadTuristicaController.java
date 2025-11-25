217669c0fa0c40b93358215e363b69daa6f06aa7
Mona Lisa <juanchit300y@gmail.com>
2025-11-25 14:35:01 -0500

    Turno controller

--- AgenciaViajess/src/Controllers/ActividadTuristicaController.java
+++ AgenciaViajess/src/Controllers/ActividadTuristicaController.java
@@ -29,14 +29,14 @@ public class ActividadTuristicaController extends GeneralController<ActividadTur
     public ActividadTuristicaController(ActividadTuristicaRepository classData) {
         this.classData= classData;
         this.municipioData= new MunicipioController();
-        this.turnoData= new TurnoRepository();
+        this.turnoData= new TurnoController();
         this.elementoPlanData= new ElementoPlanController();
         this.actividadTuristicaData= new ActividadTuristicaRepository();
     }
     @Override
     public boolean eliminarObjeto(Integer id) {
         List<ElementoPlan> elementosPlanActividadTuristica= elementoPlanData.getElementosPlanByActividadTuristica(id);
-        List<Turno> turnosActividadTuristica= turnoData.findTurnosByActividadTuristicaId(id);        
+        List<Turno> turnosActividadTuristica= turnoData.getTurnosByActividadTuristica(id);        
         if (!elementosPlanActividadTuristica.isEmpty()) {
             return false; 
         }
@@ -105,18 +105,18 @@ public class ActividadTuristicaController extends GeneralController<ActividadTur
     //Turno Relacion
     //En caso Profesor (La clase tiene un listado de objetos
     public List<Turno> getTurnosDeActividadTuristica(Integer actividadTuristicaId){ 
-        return turnoData.findTurnosByActividadTuristicaId(actividadTuristicaId);
+        return turnoData.getTurnosByActividadTuristica(actividadTuristicaId);
     }
     public boolean assignTurnoToActividadTuristica(Integer actividadTuristicaId, Integer turnoId) {
         ActividadTuristica actividadTuristica = classData.findATById(actividadTuristicaId);
-        Turno turno = turnoData.findATById(turnoId);
+        Turno turno = turnoData.getGeneralById(turnoId);
         
         if (actividadTuristica == null || turno == null) {
             return false;
         }
         
         turno.setActividadTuristicaId(actividadTuristicaId);
-        turnoData.saveT(turno);
+        turnoData.saveT(turno);// este es el error que se soluciona con otro data arriba uwu
         return true;
     }
     // Municipio Relacion
--- AgenciaViajess/src/Controllers/TurnoController.java
+++ AgenciaViajess/src/Controllers/TurnoController.java
@@ -4,11 +4,97 @@
  * and open the template in the editor.
  */
 package Controllers;
+import Persistencia.*;
+import Modelos.*;
+import java.util.List;
+
 
 /**
  *
- * @author Juan Guerrero
+ * @author DELL
  */
-public class TurnoController {
+public class TurnoController extends GeneralController<Turno> {
+    ActividadTuristicaController actividadTuristicaData;
+    GuiaController guiaData;
+    TurnoRepository turnoData;
+    public TurnoController() {
+        this.classData= new TurnoRepository();
+        this.actividadTuristicaData= new ActividadTuristicaController();
+        this.guiaData= new GuiaController();
+        this.turnoData= new TurnoRepository();
+    }
+    public TurnoController(TurnoRepository classData) {
+        this.classData= classData;
+        this.actividadTuristicaData= new ActividadTuristicaController();
+        this.guiaData= new GuiaController();
+        this.turnoData= new TurnoRepository();
+    }
+    @Override
+    public boolean eliminarObjeto(Integer id) {
+        classData.deleteT(id);
+        return true;
+    }    
     
+    public boolean actualizarTurno(Integer id, Integer actividadTuristicaId, Integer guiaId) {
+        Turno turno = classData.findATById(id);
+        if (turno == null) {
+            return false;
+        }
+        
+        if (actividadTuristicaId != null) {
+            ActividadTuristica actividadTuristica = actividadTuristicaData.getGeneralById(actividadTuristicaId);
+            if (actividadTuristica == null) {
+                return false;
+            }
+            turno.setActividadTuristicaId(actividadTuristicaId);
+        }
+        if(guiaId!= null){
+            Guia guia= guiaData.getGeneralById(guiaId);
+            if(guia== null){
+                return false;
+            }
+            turno.setGuiaId(guiaId);
+        }        
+        classData.saveT(turno);
+        return true;
+    }
+
+    public boolean añadirTurno(Integer actividadTuristicaId, Integer guiaId) {
+        ActividadTuristica actividadTuristica = actividadTuristicaData.getGeneralById(actividadTuristicaId);
+        if (actividadTuristica == null) {
+            return false;
+        }
+        Guia guia = guiaData.getGeneralById(guiaId);
+        if (guia == null) {
+            return false;
+        }
+        Turno turno = new Turno();
+        turno.setActividadTuristicaId(actividadTuristicaId);
+        turno.setGuiaId(guiaId);
+        
+        classData.saveT(turno);
+        return true;
+    }
+    //Cliente relacion (caso curso) - relacion a guia
+    public List<Turno> getTurnosByGuia(Integer guiaId) {
+        return guiaData.getTurnosDeGuia(guiaId);
+    }
+    public Guia getGuiaDeTurno(Integer turnoId) {
+        Turno turno = classData.findATById(turnoId);
+        if (turno == null) {
+            return null;
+        }
+        return guiaData.getGeneralById(turno.getGuiaId());
+    }
+    //Viaje relacion (caso curso) -  relacion a Actividad Turistica
+    public List<Turno> getTurnosByActividadTuristica(Integer actividadTuristicaId) {
+        return turnoData.findTurnosByActividadTuristicaId(actividadTuristicaId);
+    }
+    public ActividadTuristica getActividadTuristicaDeTurno(Integer turnoId) {
+        Turno turno = classData.findATById(turnoId);
+        if (turno == null) {
+            return null;
+        }
+        return actividadTuristicaData.getGeneralById(turno.getActividadTuristicaId());
+    }
 }
